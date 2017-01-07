ad3e72e02749d4812f212ff901d5e936f1abd607
yar7285 <yar7285@gmail.com>
2017-01-07 23:45:46 +0200

    modal window part1

---
+++ src/components/modal/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Yaroslav on 05.01.2017.
+ */
+export Modal from './component';
+export ModalReducer from './reducers';
+
+export {openModal, closeModal} from './actions';
--- src/reducers.js
+++ src/reducers.js
@@ -3,10 +3,11 @@ import { routerReducer } from 'react-router-redux';
 
 import { HomeReducer } from './pages/home/index';
 import { ListReducer } from './pages/list/index';
-
+import {ModalReducer} from './components/modal/index';
 
 export default combineReducers({
     routing: routerReducer,
     ...HomeReducer,
-    ...ListReducer
+    ...ListReducer,
+    ...ModalReducer
 });
